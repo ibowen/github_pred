4c42d2007de00d022fba37c80c61ea69ef0b57c5
mingchen <mingchen7@email.arizona.edu>
2016-09-22 22:32:19 -0700

    repos with homepage'

--- base/base.py
+++ base/base.py
@@ -125,7 +125,7 @@ class RepositoryManager:
             now = datetime.datetime.now()
             for repo in g.get_repos():
                 try:
-                    print repo.id, repo.name, repo.stargazers_count, repo.created_at.strftime("%Y-%m-%d %H:%M:%S"), repo.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+                    print repo.id, repo.name, repo.stargazers_count, repo.created_at.strftime("%Y-%m-%d %H:%M:%S"), repo.updated_at.strftime("%Y-%m-%d %H:%M:%S"), 
                     writer.writerow([repo.id, repo.name, repo.stargazers_count, repo.created_at, repo.updated_at])                    
                     # if repo.stargazers_count > self.STAR_THRESHOLD and (now - repo.updated_at).total_seconds > self.SECONDS_ONE_YEAR / 4:
                     #     writer.writerow([repo.id, repo.name, repo.stargazers_count, repo.created_at, repo.updated_at])                    
@@ -162,24 +162,37 @@ class RepositoryManager:
     def get_org_repos(self):
         orgs = self.load_organizaitons()
         g = Github(login_or_token=self.TOKEN, per_page=100)
+        
         with open('repos.csv','wb') as f:
             now = datetime.datetime.now()
 
             writer = csv.writer(f)
             writer.writerow(['org_id','repo_id','name','full_name','fork','forks_count','size','has_issues','has_wiki', \
-                                'language','stargazers_count','watchers_count','created_at','updated_at'])            
-            for org in orgs:            
-                organization = g.get_organization(org)
-                for repo in organization.get_repos():
-                    print repo.id, repo.name
-                    if repo.stargazers_count > 100 and (now - repo.updated_at).total_seconds > self.SECONDS_ONE_YEAR / 12:
-                            writer.writerow([org, repo.id, repo.name, repo.full_name, repo.fork, repo.forks_count, repo.size, repo.has_issues, repo.has_wiki, \
-                                repo.language, repo.stargazers_count, repo.watchers_count, repo.created_at, repo.updated_at])
+                                'language','stargazers_count','watchers_count','created_at','updated_at','homepage'])            
+            for org in orgs:
+                try:            
+                    organization = g.get_organization(org)
+                    for repo in organization.get_repos():
+                        print repo.id, repo.name, repo.homepage
+                        if repo.fork == False and repo.stargazers_count > 100 and (now - repo.updated_at).total_seconds > self.SECONDS_ONE_YEAR / 12:
+                                writer.writerow([org, repo.id, repo.name, repo.full_name, repo.fork, repo.forks_count, repo.size, repo.has_issues, repo.has_wiki, \
+                                    repo.language, repo.stargazers_count, repo.watchers_count, repo.created_at, repo.updated_at, repo.homepage])
 
-                            time.sleep(0.5)
+                                # time.sleep(0.5)
+
+                except RateLimitExceededException:
+                    print "RateLimitExceedException occurred..."
+                    time.sleep(1800)                    
+                    continue                
+
+                except BadCredentialsException:
+                    print "BadCredentialException occurred..."
+                    sys.exit(1)
+
+                except GithubException:
+                    print "Unknwon Exception occurred, skip on this one."                    
+                    sys.exit(1)              
                             
-
-
 if __name__ == '__main__':
-    access_token = '996443c69383bc84ed89859141a39b9f32737c1a'
+    access_token = ''
     RepositoryManager(access_token).get_org_repos()
